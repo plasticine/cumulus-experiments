5156b45093ca01acd92045e94d904f62fe96e325
Josh Bassett <josh.bassett@gmail.com>
2011-08-22 07:00:17 +1000

    Added a nodejs test example.

---
+++ cumulus-nodejs/test.js
@@ -0,0 +1,51 @@
+{"inputs": "test", "query": [
+  {
+    "map": {
+      "language": "javascript",
+      "source": "
+function(value, keyData, arg) {
+  return value.values.map(function(value) {
+    var result = {};
+    var fact = JSON.parse(value.data);
+    var timestamp = Math.floor(fact.timestamp / 60) * 60;
+    var key = timestamp + '_' + fact.resource;
+    result[key] = {
+      timestamp: timestamp,
+      resource: fact.resource,
+      sum_response_time: fact.response_time,
+      avg_response_time: fact.response_time,
+      min_response_time: fact.response_time,
+      max_response_time: fact.response_time,
+      num_requests: 1
+    };
+    return result;
+  });
+}"
+    }
+  }, {
+    "reduce": {
+      "keep": true,
+      "language": "javascript",
+      "source": "
+function(values, arg) {
+  return [
+    values.reduce(function(acc, value) {
+      for (var key in value) {
+        var fact = value[key];
+        if (key in acc) {
+          acc[key].sum_response_time = acc[key].sum_response_time + fact.sum_response_time;
+          acc[key].avg_response_time = ((acc[key].avg_response_time * acc[key].num_requests) + (fact.avg_response_time * fact.num_requests)) / (acc[key].num_requests + fact.num_requests);
+          acc[key].min_response_time = Math.min(acc[key].min_response_time, fact.min_response_time);
+          acc[key].max_response_time = Math.max(acc[key].max_response_time, fact.max_response_time);
+          acc[key].num_requests      = acc[key].num_requests + fact.num_requests;
+        } else {
+          acc[key] = fact;
+        }
+      }
+      return acc;
+    }, {})
+  ];
+}"
+    }
+  }
+]}
---
+++ cumulus-nodejs/test.sh
@@ -0,0 +1,3 @@
+curl -X POST -H "Content-Type: application/json" -d "{\"response_time\": 1, \"resource\":\"index\"}" http://localhost:4000/facts
+curl -X POST -H "Content-Type: application/json" -d "{\"response_time\": 2, \"resource\":\"index\"}" http://localhost:4000/facts
+curl -X POST -H "Content-Type: application/json" -d "{\"response_time\": 3, \"resource\":\"index\"}" http://localhost:4000/facts
