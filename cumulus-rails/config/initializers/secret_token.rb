59c572ec4798c0575d589b8bef7373fd0c9654a4
Josh Bassett <josh.bassett@gmail.com>
2014-03-13 07:29:54 +1100

    Updated to rails 4.

--- cumulus-rails/config/initializers/secret_token.rb
+++ cumulus-rails/config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Cumulus::Application.config.secret_token = 'f3c5b8b5be741f63b82c67d4492a634452d4ec9802aadb6f1b1fc44c76926da24e933c20a842944e9f71c9c08fac8f8d3cb499111121006bf53cfde940a382b9'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cumulus::Application.config.secret_key_base = 'ff248d3dd55443cbe717e261ba01550c886455c178d477d06a372c90951bee946106a2c4bb4fad92ca746ceaa137ee46b10d8d884af6ba53af101cd404431375'
