0e66dcc1abfaad0c5caef23af772286cd7b01d10
Josh Bassett <josh.bassett@gmail.com>
2014-03-14 12:30:40 +1100

    Seed from the current timestamp.

--- cumulus-rails/db/seeds.rb
+++ cumulus-rails/db/seeds.rb
@@ -9,11 +9,11 @@ Sequel::Model.db.execute <<-SQL
   INSERT INTO account_#{account.id}.metric_#{metric.id} (timestamp, count, resource, response_time, render_time)
 
   SELECT
-    current_date - ((n - 1) * interval '1 second'),
+    current_timestamp - ((n - 1) * interval '1 second'),
     random() * 100,
     (array['/', '/contact', '/about'])[round(random() * 2) + 1],
     random() * 100,
-    random() * 100
+    random() * 1000
 
   FROM generate_series(1, 1000000) n;
 SQL
